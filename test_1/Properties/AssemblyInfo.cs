fd1d5ff2b73d7d0bbc0ae33b8e2708aceffb2b6b
lcw <lcw@MPC3062>
2023-03-28 16:31:14 +0200

    Ajoutez des fichiers projet.

---
+++ test_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("test_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9DCE31AE-747A-4564-A0ED-52AC68D7875E")]
+[assembly: AssemblyVersion("1.0.0.0")]
